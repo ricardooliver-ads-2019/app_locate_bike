afff05a543cd6441adfa9d8210394a97457002de
RICARDO OLIVEIRA <ricardooliver.ads.2019@gmail.com>
2021-09-08 16:46:43 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# app_locate_bike
+ Aplicação desenvolvida como atividades para a materia de Programação Mobile II,
---
+++ android/app/src/main/kotlin/com/example/app_locate_bike/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.app_locate_bike
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
